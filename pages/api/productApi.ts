df8f6e34a855c9a815f693a50434bd0fce665a9c
ksm <hpk5802@github.com>
2025-01-07 21:01:24 +0900

    [refactor]: 상품 추가 api 수정

--- pages/api/productApi.ts
+++ pages/api/productApi.ts
@@ -62,22 +62,104 @@ const fetchInquiryById = async (id: string, cursor: string | null = null) => {
   }
 };
 
-const postProduct = async ({ content }: { content: PostProductInterface }) => {
+const retryFetch = async (
+  url: string,
+  options: RequestInit
+): Promise<Response> => {
+  const response = await fetch(url, options);
+
+  if (response.status === 401) {
+    // status === 401 : Unauthorized 토큰 갱신
+    const refreshResponse = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/refresh-token`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          refreshToken: localStorage.getItem("refreshToken"),
+        }),
+      }
+    );
+
+    if (refreshResponse.ok) {
+      const { accessToken, refreshToken } = await refreshResponse.json();
+      // localStorage에 갱신된 토큰 저장
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("refreshToken", refreshToken);
+
+      // requset 재요청
+      const newOptions = {
+        ...options,
+        headers: {
+          ...options.headers,
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+      return await fetch(url, newOptions);
+    } else {
+      throw new Error("토큰 갱신 실패. 다시 로그인하세요.");
+    }
+  }
+
+  return response;
+};
+
+const uploadImage = async (file: string) => {
+  const formData = new FormData();
+  formData.append("image", file);
+
   try {
-    const response = await fetch(
+    const response = await retryFetch(
+      `${process.env.NEXT_PUBLIC_UPLOAD_IMAGE_URL}`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+        body: formData,
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("업로드 실패");
+    }
+
+    const data = await response.json(); // 서버의 JSON 응답
+    return data.url; // 서버 URL 리턴해서 postArticle API에서 사용
+  } catch (error) {
+    console.error("이미지 업로드 중 오류 발생:", error);
+    return null;
+  }
+};
+
+const postProduct = async ({ content }: { content: PostProductInterface }) => {
+  let imageUrl = null;
+  if (content.images) {
+    // 이미지 업로드 후 URL 받기
+    imageUrl = await uploadImage(content.images[0]);
+    content.images = [imageUrl];
+  }
+
+  try {
+    const response = await retryFetch(
       `${process.env.NEXT_PUBLIC_SERVER_URL}/products`,
       {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          // Authorization: 'Jwt Token'
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
         },
         body: JSON.stringify(content),
       }
     );
+
     if (!response.ok) {
       throw new Error("상품 추가 실패");
     }
+
+    return response.json();
   } catch (error) {
     throw error;
   }
